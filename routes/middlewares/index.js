8abf96629737ff6535a2d849140e01946e41217d
Yannick <yannick.bezes@gmail.com>
2018-11-23 14:16:18 +0100

    add middleware cors

--- routes/middlewares/index.js
+++ routes/middlewares/index.js
@@ -1,8 +1,10 @@
 import bodyParser from 'body-parser'
 import logger from './logger'
 import express from 'express'
+import cors from 'cors'
 
 export default (app) => {
+    app.use(cors())
     app.use(logger)
     app.use(express.json())
     app.use(bodyParser.urlencoded({
